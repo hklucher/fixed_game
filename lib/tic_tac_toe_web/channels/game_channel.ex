73955593f8c59f7cc94bad1ffcb0e8c45b706b35
Harrison Klucher <hklucher@gmail.com>
2017-10-15 21:18:35 -0500

    track current user ID in js

--- lib/tic_tac_toe_web/channels/game_channel.ex
+++ lib/tic_tac_toe_web/channels/game_channel.ex
@@ -2,8 +2,10 @@ require IEx
 
 defmodule TicTacToeWeb.GameChannel do
   use Phoenix.Channel
+  alias TicTacToe.Presence
 
   def join("game", _payload, socket) do
+    #send(self(), :after_join)
     {:ok, assign(socket, :marker, "X")}
   end
 
@@ -14,6 +16,14 @@ defmodule TicTacToeWeb.GameChannel do
 
   def handle_in("get_marker", _params, socket) do
     broadcast(socket, "get_marker", %{marker: "X"})
+    {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+    push socket, "presence_state", Presence.list(socket)
     {:noreply, socket}
   end
 end

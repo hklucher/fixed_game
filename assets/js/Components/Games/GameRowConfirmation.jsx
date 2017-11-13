e93a465490910a1d7e7bc508b192de929e787e5c
Harrison Klucher <hklucher@gmail.com>
2017-11-12 18:18:24 -0600

    WIP - begin animation transitions on game index:

--- assets/js/Components/Games/GameRowConfirmation.jsx
+++ assets/js/Components/Games/GameRowConfirmation.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import PropTypes from 'prop-types';
 
 const GameRowConfirmation = (props) => {
   if (props.visible) {
     return (
-      <div className="confirmation">
-        <div className="confirmation__header">
-          <p className="confirmation__header-text">
-            Are you sure you want to join this game?
-          </p>
+      <ReactCSSTransitionGroup>
+        <div className="confirmation">
+          <div className="confirmation__header">
+            <p className="confirmation__header-text">
+              Are you sure you want to join this game?
+            </p>
+          </div>
+
+          <div className="confirmation__options">
+            <button className="btn btn-primary confirmation__options-action">
+              Yes, let's go!
+            </button>
+
+            <button className="btn btn-primary confirmation__options-action">
+              Not quite yet.
+            </button>
+          </div>
+
         </div>
-
-        <div className="confirmation__options">
-          <button className="btn btn-primary confirmation__options-action">
-            Yes, let's go!
-          </button>
-
-          <button className="btn btn-primary confirmation__options-action">
-            Not quite yet.
-          </button>
-        </div>
-
-      </div>
+      </ReactCSSTransitionGroup>
     );
   }
 
